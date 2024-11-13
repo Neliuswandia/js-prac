0162047a5667f12b761a29d949535e7dd948ad59
Neliuswandia <nelius.wandia@moringaschool.com>
2024-11-13 19:45:43 +0300

    styling elements in js

--- dom.js
+++ dom.js
@@ -1,27 +1,40 @@
 // ###########  Selecting elements  ###################
 
 // getElementById()
-const shop = document.getElementById("shop"); // shop being the id
+// const shop = document.getElementById("shop"); // shop being the id
 
 
 // getElementByClassName()
-const menuCards = document.getElementsByClassName("menuCards"); // menuCards being the class name
+// const menuCards = document.getElementsByClassName("menuCards"); // menuCards being the class name
 
 // getElementByTagName()
-const li = document.getElementsByTagName("li"); // li being the tag
+// const li = document.getElementsByTagName("li"); // li being the tag
 
 // querySelector()
-const div = document.getElementByQuerySelector("div"); // pics the first element of the same kind elements
+// const div = document.getElementByQuerySelector("div"); // pics the first element of the same kind elements
 
 // querySelectorAll()
-const button = document.getElementByQuerySelector("button"); // pics the all the elements that are the same
+// const button = document.getElementByQuerySelector("button"); // pics the all the elements that are the same
 
-function colorChanger(){
-//  const mainHeading = document.getElementById("mainHeading").innerHTML = "This is Nell"
-    const mainHeading = document.getElementById("mainHeading")
-    if (mainHeading.innerHTML === "Introduction to DOM"){
-        mainHeading.innerHTML = "This is Nell"
-    }else {
-        mainHeading.innerHTML = "Introduction to DOM"
-    }
-}
+// function colorChanger(){
+// //  const mainHeading = document.getElementById("mainHeading").innerHTML = "This is Nell"
+//     const mainHeading = document.getElementById("mainHeading")
+//     if (mainHeading.innerHTML === "Introduction to DOM"){
+//         mainHeading.innerHTML = "This is Nell"
+//     }else {
+//         mainHeading.innerHTML = "Introduction to DOM"
+//     }
+// }
+
+// ###########  Styling elements  ###################
+document.getElementById("myH1").style.color = "blue";
+
+document.getElementById("mainHeading").style.color = "red"
+mainHeading.style.fontSize = "5rem"
+console.log(mainHeading) 
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("myH1").style.color = "blue";
+//     document.getElementById("mainHeading").style.color = "red";
+// });
+
