7bd980442166df6f6628962c4baf61886d32ad96
Neliuswandia <nelius.wandia@student.moringaschool.com>
2025-01-21 17:48:37 +0300

    asynchronous callback

--- callback.js
+++ callback.js
@@ -1,4 +1,33 @@
-function greet(name, callback){
-    console.log(`Hello, ${name}!`);
-    callback();
+// // ############ Synchronous callback (operates line by line) ###############
+// function greet(name, callback){
+//     console.log(`Hello, ${name}!`);
+//     callback();
+// }
+
+// function sayGoodbye(){
+//     console.log("Goodbye!");
+// }
+
+// greet("John", sayGoodbye());
+
+// ############ Asynchronous callback (operates concurrently) ###############
+
+
+let pizza
+function orderPizza(){
+    console.log("Oder Pizza")
+    setTimeout(()=> {
+        pizza = "Margherita"
+    }, 2000)
+    console.log("waiting.........")
+    console.log(`Your ${pizza} is here`)
 }
+orderPizza()
+console.log("Do my laundry")
+console.log("Do my studies")
+
+setTimeout(() => {
+    console.log(`Your ${pizza} is here`)
+},2000)
+
+
