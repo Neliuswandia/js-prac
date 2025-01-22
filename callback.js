1fd7e9758a0de122087ed3a22b08f94b1c1e4525
Neliuswandia <nelius.wandia@student.moringaschool.com>
2025-01-22 10:22:27 +0300

    using call backs

--- callback.js
+++ callback.js
@@ -10,24 +10,43 @@
 
 // greet("John", sayGoodbye());
 
+
 // ############ Asynchronous callback (operates concurrently) ###############
 
 
-let pizza
-function orderPizza(){
-    console.log("Oder Pizza")
-    setTimeout(()=> {
-        pizza = "Margherita"
-    }, 2000)
-    console.log("waiting.........")
+// let pizza
+// function orderPizza(){
+//     console.log("Oder Pizza")
+//     setTimeout(()=> {
+//         pizza = "Margherita"
+//     }, 2000)
+//     console.log("waiting.........")
+//     console.log(`Your ${pizza} is here`)
+// }
+// orderPizza()
+// console.log("Do my laundry")
+// console.log("Do my studies")
+
+// setTimeout(() => {
+//     console.log(`Your ${pizza} is here`)
+// },2000)
+
+
+// ############ Using the callback ###############
+
+
+function orderPizza(callback){
+    setTimeout(() => {
+        console.log("waiting.........")
+        const pizza = "chicken"
+        callback(pizza)
+        
+    },2000)
+}
+
+function pizzaReady(pizza) {
     console.log(`Your ${pizza} is here`)
 }
-orderPizza()
-console.log("Do my laundry")
-console.log("Do my studies")
 
-setTimeout(() => {
-    console.log(`Your ${pizza} is here`)
-},2000)
-
-
+orderPizza(pizzaReady)
+console.log('Call a friend')
